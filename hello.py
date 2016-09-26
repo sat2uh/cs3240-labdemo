89053baab2a179d53da20b3434d3090a2802f1ea
Simonne <sat2uh@virginia.edu>
2016-09-26 15:12:11 -0400

    Activity 1, step 4: commit to local repo

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-# testing... for GitHub
+# Simonne ter Weele
+# sat2uh
+import fileinput
+import sys
+
+def greetings(msg):
+    print(msg)
+
+def main():
+    aGreet = input("Enter a greeting: ")
+    greetings(aGreet)
+
+if __name__ == "__main__":
+    main()
